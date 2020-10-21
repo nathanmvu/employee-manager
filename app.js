8dbb928b0260ab446f2652395d8d9c4528acde24
Nathan Vu <nathanvu99@gmail.com>
2020-10-20 21:29:37 -0700

    Working view and add functions

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+//Content Managment Systems (CMS)
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-const inquirer = require('inquirer');
-const mysql = require('mysql');
-const { allowedNodeEnvironmentFlags } = require('process');
-
-const db = new Database({
+// create the connection information for the sql database
+var connection = mysql.createConnection({
   host: "localhost",
-  port: 3000,
+  port: 3306,
   user: "root",
   password: "password",
   database: "employeeDB"
 });
 
-db.connect(function(err) {
+connection.connect(function(err) {
   if (err) throw err;
+  console.log('Connected as id ' + connection.threadId + '\n');
   initPrompt();
 })
 
@@ -28,7 +29,7 @@ initPrompt = () => {
         'View Roles', 
         'View Employees', 
         'Update Employee', 
-        'Quit'],
+        'QUIT'],
       message: 'What would you like to do?',
       name: 'userOptions'
     }]).then(function(response) {
@@ -55,47 +56,147 @@ initPrompt = () => {
         updateEmployee(); 
         break;
       default:
+        connection.end
         break;
     }
   });
 }
 
 addDepartment = () => {
+  console.log("Inserting a new department.\n");
   inquirer.
   prompt([{
     type: 'input',
     name: 'department',
     message: 'What department would you like to add?: '
-  }]).then(function(response) {
-    let query = db.query('INSERT INTO department SET ?', {department: res.name},
+  }]).then(function(res) {
+    let query = connection.query('INSERT INTO department SET ?', {name: res.department},
     function(err, res) {
       if (err) throw err;
-      console.table(res);
-      initPrompt();
+      connection.query('SELECT * FROM department', function (err, res){
+        console.table(res);
+        initPrompt();
+      });
     });
   });
 }
 
 addRole = () => {
+  console.log("Inserting a new role.\n");
+  const departments = [];
+  connection.query('SELECT * FROM department', function(err, res) {
+    if (err) throw err;
+    for(let i = 0; i < res.length; i++) {
+      departments.push({name: res[i].name, value: res[i].id});
+    }
+  })
   inquirer
-  .prompt([{
-    type: 'input',
-    name: 'roleName',
-    message: 'What role would you like to add?: '
-  }]).then(function(response) {
-    let query = db.query('INSERT INTO role SET ?', {roleName: res.name},
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the role to add? '
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'What is the salary of the role? '
+    },
+    {
+      type: 'list',
+      name: 'deptID',
+      choices: departments,
+      message: 'What is the ID of the department this role belongs to? '
+    }
+  ]).then(function(res) {
+    connection.query('INSERT INTO role SET ?',
+    {
+      title: res.title,
+      salary: res.salary,
+      department_id: res.deptID
+    },
     function(err, res) {
       if (err) throw err;
-      console.table(res);
       initPrompt();
     });
   });
 }
 
+selectRole = () => {
+
+}
+
 addEmployee = () => {
-  
+  console.log("Inserting a new employee.\n");
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter employee first name: '
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter employee last name: '
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: 'Enter employee last name: '
+    },
+    {
+      type: 'input',
+      name: 'managerName',
+      message: 'Who is their manager?'
+    }
+  ]).then(function(res) {
+    let query = connection.query('INSERT INTO employees SET ?', res,
+    function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      initPrompt();
+    })
+  })
 }
 
 viewDepartments = () => {
-  
+  connection.query('SELECT * FROM department', function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    initPrompt();
+  })
+}
+
+viewRoles = () => {
+  connection.query('SELECT * FROM role LEFT JOIN department ON department.id = role.department_id', function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    initPrompt();
+  })
+}
+
+updateEmployee = () => {
+  connection.query('SELECT first_name, last_name, id FROM employees', function(err, res) {
+    if (err) throw err;
+    let employeeInfo = res.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}))
+    inquirer
+    .prompt([{
+      type: 'list',
+      name: 'employees',
+      choices: employeeInfo
+    },
+    {
+      type: 'input',
+      name: 'newRole',
+      message: "What is the employee's new role? "
+    }
+    ]).then(function(res) {
+      connection.query('UPDATE employee SET role_ID = ? WHERE first_name = ?', [res.employees, res.newRole], function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        initPrompt();
+      });
+    });
+  });
 }
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
 DROP DATABASE IF EXISTS employeeDB;
-
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
@@ -13,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(9,2) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (id)
 );
@@ -25,4 +24,16 @@ CREATE TABLE employee (
   role INT,
   manager_id INT NULL,
   PRIMARY KEY (id)
-)
+);
+
+--- Department filler ---
+INSERT INTO department (name)
+VALUE ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+--- Role filler ---
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 95000, 1), ('Sales Analyst', 80000, 1), ('UI/UX Designer', 85000, 2), ('Software Engineer', 125000, 2), ('Accountant', 80000, 3), ('Legal Representative', 100000, 4);
+
+--- Employee filler ---
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jonas', 'Franck', 1, null), ('Eshan', 'Mcmanus', 1, 1), ('Priyanka', 'Garg', 2, 2);
